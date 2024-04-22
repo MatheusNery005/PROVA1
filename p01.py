b700cb1382fe7d1ba727e41aa504b4b8033b809c
MatheusNery005 <164574641+MatheusNery005@users.noreply.github.com>
2024-04-22 20:41:09 -0300

    Add files via upload

---
+++ p01.py
@@ -0,0 +1,17 @@
+a = float(input('Escreva um número: '))
+b = float(input('Escreva um número: '))
+c = float(input('Escreva um número: '))
+
+if a >= b and a >= c and b >= c:
+    print(f'A ordem decrescente é {a} , {b} e {c}')
+elif c >= a and c >= b and b >= a:
+    print(f'A ordem decrescente é {c} , {b} e {a}')
+elif b >= a and b >= c and a >= c:
+    print(f'A ordem decrescente é {b} , {a} e {c}')
+elif b >= a and b >= c and c >= a:
+    print(f'A ordem decrescente é {b} , {c} e {a}')
+elif c >= a and c >= b and a >=b:
+    print(f'A ordem decrescente é {c} , {a} e {b}')
+elif a >= b and a >=c and c >= b:
+    print(f'A ordem decrescente é {a} , {c} e {b}')
+
